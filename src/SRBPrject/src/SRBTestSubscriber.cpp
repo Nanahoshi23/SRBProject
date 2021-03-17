8ca37c8dfd5c88b58d46b7e90e114188d14fa38a
Nanahoshi <nanahoshi.931@gmail.com>
2021-03-17 03:32:04 +0000

    feat: Edit SRBTestSubscriber.cpp #6
    テスト用のSubscriberを実装しました．

---
+++ src/SRBPrject/src/SRBTestSubscriber.cpp
@@ -0,0 +1,18 @@
+#include <ros/ros.h>
+#include <std_msgs/String>
+
+void msgCallBack(const std_msgs::String::ConstPtr& msg)
+{
+    ROS_INFO("I heard: [%s]", msg->data.c_str()) ;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "SRBTestSubscriber") ;
+    ros::NodeHandle nh ;
+
+    ros::Subscriber srb_test_sub = nh.subscribe("SRBTestSubscriber", 1000, msgCallBack) ;
+    ros::spin() ;
+
+    return() ;
+}
